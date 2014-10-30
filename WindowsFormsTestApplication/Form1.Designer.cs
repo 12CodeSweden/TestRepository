c2c91bb7a9cd0f334d9dbc837192be5c098a9ee7
benblade <benny.bladin@gmail.com>
2014-10-30 17:27:46 +0100

    Added message box with a hello message.

--- WindowsFormsTestApplication/Form1.Designer.cs
+++ WindowsFormsTestApplication/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonHello = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(13, 13);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(257, 22);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // buttonHello
+            // 
+            this.buttonHello.Location = new System.Drawing.Point(95, 63);
+            this.buttonHello.Name = "buttonHello";
+            this.buttonHello.Size = new System.Drawing.Size(75, 39);
+            this.buttonHello.TabIndex = 1;
+            this.buttonHello.Text = "Hello";
+            this.buttonHello.UseVisualStyleBackColor = true;
+            this.buttonHello.Click += new System.EventHandler(this.buttonHello_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.buttonHello);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button buttonHello;
     }
 }
 
--- WindowsFormsTestApplication/Form1.cs
+++ WindowsFormsTestApplication/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsTestApplication
         {
             InitializeComponent();
         }
+
+        private void buttonHello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello " + this.textBoxName.Text);
+        }
     }
 }
