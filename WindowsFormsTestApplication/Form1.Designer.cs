d32eaeb566ebd46228c6b81b3ef6326f9c91c217
benblade <benny.bladin@gmail.com>
2014-11-03 13:45:20 +0100

    Improved UI

--- WindowsFormsTestApplication/Form1.Designer.cs
+++ WindowsFormsTestApplication/Form1.Designer.cs
@@ -30,30 +30,41 @@
         {
             this.textBoxName = new System.Windows.Forms.TextBox();
             this.buttonHello = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(13, 13);
+            this.textBoxName.Location = new System.Drawing.Point(13, 29);
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.Size = new System.Drawing.Size(257, 22);
             this.textBoxName.TabIndex = 0;
             // 
             // buttonHello
             // 
-            this.buttonHello.Location = new System.Drawing.Point(95, 63);
+            this.buttonHello.Location = new System.Drawing.Point(12, 57);
             this.buttonHello.Name = "buttonHello";
-            this.buttonHello.Size = new System.Drawing.Size(75, 39);
+            this.buttonHello.Size = new System.Drawing.Size(258, 39);
             this.buttonHello.TabIndex = 1;
             this.buttonHello.Text = "Hello";
             this.buttonHello.UseVisualStyleBackColor = true;
             this.buttonHello.Click += new System.EventHandler(this.buttonHello_Click);
             // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 9);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(45, 17);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Name";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.ClientSize = new System.Drawing.Size(282, 125);
+            this.Controls.Add(this.labelName);
             this.Controls.Add(this.buttonHello);
             this.Controls.Add(this.textBoxName);
             this.Name = "Form1";
@@ -67,6 +78,7 @@
 
         private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.Button buttonHello;
+        private System.Windows.Forms.Label labelName;
     }
 }
 
